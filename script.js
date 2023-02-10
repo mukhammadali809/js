cf6433e6ba44e8ece02186feef5b906ac0c3e2a1
Muhammadali <mukhammadali809@gmail.com>
2023-02-10 22:37:30 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let ism = prompt("ismingizni kiriting")
+let yil = prompt("hozirgi yilni kiriting")
+let yosh = prompt("tugilgan yilingizni kiriting")
+let now = yil - yosh
+
+console.log("Sizning ismingiz " + ism)
+console.log("Sizning yoshingiz esa: " + now)
